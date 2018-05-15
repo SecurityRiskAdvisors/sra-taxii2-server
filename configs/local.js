47f358c1fed4bc2b4db6a28aa91d5bfa53e46b12
thebleucheese <30511683+thebleucheese@users.noreply.github.com>
2018-05-15 12:09:54 -0400

    Add docker compose, ability to quickly get running server started

--- configs/local.js
+++ configs/local.js
@@ -5,14 +5,14 @@ const localConfig = {
     httpPort: 3000,
     httpsPort: 3001,
     viewDir: './app/views',
-    connectionString: "mongodb://mongo:27017/",
+    connectionString: "mongodb://sra-taxii2-mongo:27017/",
     // if you're using a taxii management application it needs to point to the same conf DB to manage this instance
     confDb: "taxii2config",
     certDir: "/opt/taxii/certs",
     stixContentType: 'application/vnd.oasis.stix+json; version=2.0',
     taxiiContentType: 'application/vnd.oasis.taxii+json; version=2.0',
     paginationLimit: 100,
-    managerUrl: 'https://sra_taxii2_manager_server:4001'
+    managerUrl: 'https://sra-taxii2-manager-server:4001'
 };
 
 module.exports = localConfig;
