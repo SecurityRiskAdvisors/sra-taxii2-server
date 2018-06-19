e788a81e0b6ef94497b1e5cbf61d7bd779069e7b
thebleucheese <30511683+thebleucheese@users.noreply.github.com>
2018-06-18 23:27:17 -0400

    Improve error handling

--- server/middleware/render-response.js
+++ server/middleware/render-response.js
@@ -3,11 +3,20 @@
 module.exports = (req, res, next) => {
     const statusCode = res.statusCode || 404;
     res.status(statusCode);
+    
+    // @TODO - this is hardcoded for now, how to handle?
+    res.data = res.data ? res.data : {
+        "title": "Not Found",
+        "description": "The requested resource could not be found.",
+        "http_status": 404
+    };
 
     // respond with html page
     if (req.accepts('html')) {
-        return res.render(res.locals.taxiiRenderTemplate, {
-            title: res.locals.taxiiRenderTitle,
+        let renderTemplate = res.locals.taxiiRenderTemplate ? res.locals.taxiiRenderTemplate : process.env.BASE_TEMPLATE
+        let renderTitle = res.locals.taxiiRenderTitle ? res.locals.taxiiRenderTitle : "Error";
+        return res.render(renderTemplate, {
+            title: renderTitle,
             data: res.data
         });
     } 
